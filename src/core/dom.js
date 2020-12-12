85764f9e46a50a6911af397e377c069e39f640a1
murash4 <qoc306a>
2020-12-12 15:28:20 +0300

    create custom jquery

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,7 +1,41 @@
-class DOM {}
+class DOM {
+	constructor (selector) {
+		this.$el = typeof selector === 'string'
+			? document.querySelector(selector)
+			: selector
+	}
 
-export function $ () {
-	return new DOM()
+	html (html) {
+		if (typeof html === 'string') {
+			this.$el.innerHTML = html
+			return this
+		}
+
+		return this.$el.outerHTML.trim()
+	}
+
+	clear () {
+		this.html('')
+		return this
+	}
+
+	append (node) {
+		if (node instanceof DOM) {
+			node = node.$el
+		}
+
+		if (Element.prototype.append) {
+			this.$el.append(node)
+		} else {
+			this.$el.appendChild(node)
+		}
+
+		return this
+	}
+}
+
+export function $ (selector) {
+	return new DOM(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -11,5 +45,5 @@ $.create = (tagName, classes = '') => {
 		el.classList.add(classes)
 	}
 
-	return el
+	return $(el)
 }
