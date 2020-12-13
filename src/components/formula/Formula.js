764007b5a6131016aa1a81a575d075f138c817cc
murash4 <qoc306a>
2020-12-13 12:42:15 +0300

    preparing a structure for events

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,21 @@ import { ExcelComponent } from '@core/ExcelComponent'
 export class Formula extends ExcelComponent {
 	static className = 'excel-formula'
 
+	constructor ($root) {
+		super($root, {
+			name: 'formula',
+			listeners: ['input']
+		})
+	}
+
 	toHTML () {
 		return `
 			<div class="excel-formula__info">fx</div>
 			<div class="excel-formula__input" contenteditable spellcheck="false"></div>
 		`
+	}
+
+	onInput (event) {
+		console.log('Formula onInput', event)
 	}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,15 @@
 import { DOMListener } from '@core/DOMListener'
 
 export class ExcelComponent extends DOMListener {
-	// Возвращат шаблон компонента
-	toHRML () {
+	constructor ($root, options = {}) {
+		super($root, options.listeners)
+	}
+	// Возвращат шаблон компонентаc
+	toHTML () {
 		return ''
+	}
+
+	init () {
+		this.initDOMListeners()
 	}
 }
