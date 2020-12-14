cedb9a9fcb95630f82b81996dca41998328cbe2e
murash4 <qoc306a>
2020-12-15 01:02:25 +0300

    resized tables

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,12 +21,23 @@ export class Table extends ExcelComponent {
 			const $resizer = $(event.target)
 			const $parent = $resizer.closest('[data-type="resizable"]')
 			const coords = $parent.getCoords()
+			const type = $resizer.data.resize
+			const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+			let value = null
 
 			document.onmousemove = e => {
-				const delta = e.pageX - coords.right
-				const value = coords.width + delta
+				if (type === 'col') {
+					const delta = e.pageX - coords.right
 
-				$parent.$el.style.width = value + 'px'
+					value = coords.width + delta
+					$parent.css({ width: value + 'px' })
+					cells.forEach(el => el.style.width = value + 'px')
+				} else {
+					const delta = e.pageY - coords.bottom
+
+					value = coords.height + delta
+					$parent.css({ height: value + 'px' })
+				}
 			}
 
 			document.onmouseup = () => {
@@ -35,3 +46,10 @@ export class Table extends ExcelComponent {
 		}
 	}
 }
+
+// 149 ms  Scripting
+// 1465 ms  Rendering
+// 121 ms  Painting
+// 1072 ms  System
+// 14473 ms  Idle
+// 17280 ms  Total
--- src/core/dom.js
+++ src/core/dom.js
@@ -48,6 +48,22 @@ class DOM {
 	getCoords () {
 		return this.$el.getBoundingClientRect()
 	}
+
+	get data () {
+		return this.$el.dataset
+	}
+
+	findAll (selector) {
+		return this.$el.querySelectorAll(selector)
+	}
+
+	css (styles = {}) {
+		Object
+			.keys(styles)
+			.forEach(key => {
+				this.$el.style[key] = styles[key]
+			})
+	}
 }
 
 export function $ (selector) {
