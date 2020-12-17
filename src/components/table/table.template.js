7fa58680ca3040f820194ba398268a401a172d55
murash4 <qoc306a>
2020-12-18 00:06:35 +0300

    render of state

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,12 +2,20 @@ const CODES = {
 	A: 65,
 	Z: 90
 }
+const DEFAULT_WIDTH = 120
 
-function toCell (row) {
+function getWidth (state, index) {
+	return (state ? state[index] : DEFAULT_WIDTH) + 'px'
+}
+
+function toCell (state, row) {
 	return function (_, col) {
+		const width = getWidth(state.colState, col)
+
 		return `
 			<div
 				class="excel-table__cell"
+				style="width: ${width}"
 				contenteditable
 				data-col="${col}"
 				data-type="cell"
@@ -16,9 +24,14 @@ function toCell (row) {
 	}
 }
 
-function toColumn (col, index) {
+function toColumn ({ col, index, width }) {
 	return `
-		<div class="excel-table__col" data-type="resizable" data-col="${index}">
+		<div
+			class="excel-table__col"
+			style="width: ${width}"
+			data-type="resizable"
+			data-col="${index}"
+		>
 			${col}
 			<div class="excel-table__col-resize" data-resize="col"></div>
 		</div>
@@ -42,12 +55,23 @@ function toChar (_, index) {
 	return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable (rowsCount = 15) {
+function withWidthFrom (state) {
+	return function (col, index) {
+		return {
+			col,
+			index,
+			width: getWidth(state.colState, index)
+		}
+	}
+}
+
+export function createTable (rowsCount = 15, state = {}) {
 	const colsCount = CODES.Z - CODES.A + 1
 	const rows = []
 	const cols = new Array(colsCount)
 		.fill('')
 		.map(toChar)
+		.map(withWidthFrom(state))
 		.map(toColumn)
 		.join('')
 
@@ -56,7 +80,7 @@ export function createTable (rowsCount = 15) {
 	for (let row = 0; row < rowsCount; row++) {
 		const cels = new Array(colsCount)
 			.fill('')
-			.map(toCell(row))
+			.map(toCell(state, row))
 			.join('')
 
 		rows.push(createRow(row + 1, cels))
