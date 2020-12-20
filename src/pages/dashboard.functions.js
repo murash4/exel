53a510106b7df6ab5afeedf74e353d7e7ed509a8
murash4 <qoc306a>
2020-12-20 23:27:32 +0300

    create links for unique excel pages

---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,44 @@
+function getAllKeys () {
+	const keys = []
+
+	for (let i = 0; i < localStorage.length; i++) {
+		const key = localStorage.key([i])
+
+		if (key && key.includes('excel')) {
+			keys.push(key)
+		}
+	}
+
+	return keys
+}
+
+function toHTML (item, index) {
+	return `
+		<li class="db__list-record">
+			<a href="" class="db__list-record-link">Таблица №1</a>
+			<strong>12.06.2020</strong>
+		</li>
+	`
+}
+
+function getAllRecords (keys) {
+	return keys.map(toHTML).join('')
+}
+
+export function createRecordsTable () {
+	const keys = getAllKeys()
+
+	if (!keys.length) {
+		return `<p>Вы пока не создали ни одной таблицы</p>`
+	}
+
+	return `
+		<div class="db__list-header">
+			<span>Название</span>
+			<span>Дата открытия</span>
+		</div>
+		<ul class="db__list">
+			${getAllRecords(keys)}
+		</ul>
+	`
+}
